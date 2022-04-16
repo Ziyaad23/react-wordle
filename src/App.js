380b2ea2a6c94d409096c67715242f4b0904b050
Ziyaad <rhyman.ziyaad@gmail.com>
2022-04-16 10:51:58 +0400

    [+][ADD] Setup project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center w-screen h-screen text-white bg-gray-900" >
+      <nav className="h-16 w-full m-0 border-gray-200 border-b grid place-items-center">
+        <h1 className="m-0 text-white text-4xl font-sans">React Wordle</h1>
+      </nav>
     </div>
   );
 }
